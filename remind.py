1e24372deee6b509d251d5f4eb277a1fd2b6a261
wozza <warwick_stone@fastmail.com>
2024-06-09 00:32:57 +1000

    WIP: time of phrase

--- remind.py
+++ remind.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import sys
 import re
 
@@ -15,6 +16,18 @@ def main():
 def get_time_phrase(str):
     a = re.findall(r'at \d+.*$', str)[-1]
     if a: return a
+
+
+def time_of(time_now: datetime, phrase: str) -> datetime:
+    if phrase.startswith('at'):
+        number = int(re.findall(r'\d+$', phrase)[0])
+        return datetime(
+            time_now.year,
+            time_now.month,
+            time_now.day,
+            12 + number
+        )
+    return datetime.now()
 
 
 if __name__ == '__main__':
--- test_remind.py
+++ test_remind.py
@@ -1,12 +1,22 @@
+from datetime import datetime
 import pytest
 
 import remind
 
-testdata = [
+time_phrases = [
     ("do thing at 5", "at 5"),
     ("do thing at 5pm", "at 5pm"),
 ]
 
-@pytest.mark.parametrize("text, expected", testdata)
+@pytest.mark.parametrize("text, expected", time_phrases)
 def test_get_time_phrase(text, expected):
     assert remind.get_time_phrase(text) == expected
+
+
+time_of_cases = [
+    (datetime(2024, 6, 8, 12, 0, 0), "at 5", datetime(2024, 6, 8, 17, 0 ,0))
+]
+
+@pytest.mark.parametrize("time_now, phrase, expected", time_of_cases)
+def test_time_of(time_now, phrase, expected):
+    assert remind.time_of(time_now, phrase) == expected
